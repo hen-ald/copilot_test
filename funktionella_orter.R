3984173c0e55b7caff1d84dad4ec6ce82d3fbe43
hen-ald <henkealden@gmail.com>
2024-01-23 21:26:45 +0100

    updaterat med sysselsättning

--- funktionella_orter.R
+++ funktionella_orter.R
@@ -186,13 +186,66 @@ processed_data <- joined_data %>%
 final_data <- processed_data %>%
   distinct(geom, .keep_all = TRUE)
 
+library(dplyr)
+
+# Adding a unique ID column to the final data
+final_data_with_id <- final_data %>%
+  mutate(unique_id = row_number())
+
+# View the updated dataframe
+glimpse(final_data_with_id)
+
+
 mapview(final_data)+
   mapview(smaort_layer, col.regions = "red") + 
   mapview(tatort_layer, col.regions = "blue") + 
   mapview(fritid_layer, col.regions = "green") + 
   mapview(anlag_layer, col.regions = "yellow")
 
-funktionella_orter <- final_data
+funktionella_orter <- final_data_with_id
+
+st_write(funktionella_orter, "funktionella_orter.gpkg", layer = "funktionella_orter", driver = "GPKG")
+
+dagbef <- st_read("dag_bef_1kmruta.gpkg") |> 
+  st_transform(3006) |> 
+  st_make_valid()
+
+#make centroids
+dagbef_centroids <- dagbef |> 
+  st_centroid()
+
+sysselsattning <- st_join(dagbef_centroids, funktionella_orter, join = st_within)
+
+# Summarize workforce data for each area
+sum_sysselsattning <- sysselsattning %>%
+  group_by(unique_id) %>%  # Replace 'area_id' with the identifier for your areas of interest
+  summarise(sum_dagbef = sum(dagbef, na.rm = TRUE))
+
+# Convert to a regular dataframe if it's an sf object
+sum_sysselsattning_df <- as.data.frame(sum_sysselsattning)
+
+final_sysselsatt <- funktionella_orter %>%
+  left_join(sum_sysselsattning_df, by = "unique_id") %>%
+  mutate(sum_dagbef = ifelse(is.na(sum_dagbef), NA, sum_dagbef))
+
+mapview(final_sysselsatt, zcol = "sum_dagbef")
+
+
+# make centroids of final_sysselsatt filter >= 100 sum_dagbef
+
+final_sysselsatt_centroids <- final_sysselsatt |> 
+  st_centroid()
+
+regionalnod_sysselsattning <- final_sysselsatt_centroids |> 
+  filter(sum_dagbef >= 100)
+
+mapview(regionalnod_sysselsattning, zcol = "sum_dagbef", cex = "sum_dagbef")+
+  mapview(tatort_layer, col.regions = "blue", alpha.regions = 0.3)
+
+
+
+
+
 
 
 
